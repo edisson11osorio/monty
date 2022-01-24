55e3bfa9947b083ae4b3a088629dbcabef94e15b
edisson11osorio <osorioedisson08@gmail.com>
2022-01-24 12:33:55 -0500

    2f

---
+++ functions_2.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+/**
+ * _add - Function that adds the top two elements of the stack.
+ * Return: Void.
+ * @head: Head of a double linked list
+ * @line_number: Number of lines executed before an error is printed
+ */
+// void _add(stack_t **head, unsigned int line_number)
+// {
+// 	int sum = 0;
+// 	stack_t *delete_node;
+
+// 	if ((*head) == NULL || (*head)->next == NULL)
+// 	{
+// 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+// 		exit(EXIT_FAILURE);
+// 	}
+// 	sum = (*head)->n + (*head)->next->n;
+// 	(*head)->next->n = sum;
+
+// 	delete_node = *head;
+// 	*head = delete_node->next;
+// 	if (*head != NULL)
+// 	{
+// 		(*head)->prev = NULL;
+// 	}
+// 	free(delete_node);
+}
