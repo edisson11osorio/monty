c7590324b80e9b18bfa3994774091e5244cf12fd
edisson11osorio <osorioedisson08@gmail.com>
2022-01-24 12:02:52 -0500

    included prototypes

--- number_verifier.c
+++ number_verifier.c
@@ -1,3 +1,23 @@
 #include "monty.h"
 /**
+ * number_verifier - Function that cheks if the content of
+ * a string is a number.
+ * Return: 0 if it's a number or 1 if not.
+ * @string: Pointer to the string to be checked
  **/
+int number_verifier(char *string)
+{
+	// if (string == NULL)
+	// {
+	// 	return (0);
+	// }
+	// if (*string == '\0')
+	// {
+	// 	return (1);
+	// }
+	// if ((*string >= '0' && *string <= '9') || *string == '-')
+	// {
+	// 	return (number_verifier(string + 1));
+	// }
+	// return (0);
+}
