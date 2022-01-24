749b15ea5adf7d77c272a068a60d4a3f5c3141e5
edisson11osorio <osorioedisson08@gmail.com>
2022-01-24 11:49:08 -0500

    satck.c

--- satck.c
+++ satck.c
@@ -1,4 +1,15 @@
 #include "monty.h"
 /**
+ * free_stack - Function that frees a stack
+ * Return: Void.
+ * @head: Head of the double linked list
  */
-
+void free_stack(stack_t *head)
+{
+	if (head == NULL)
+	{
+		return;
+	}
+	free_stack(head->next);
+	free(head);
+}
