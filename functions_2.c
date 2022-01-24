639dabfddaf497d308aa4e6268a670738617f6a4
edisson11osorio <osorioedisson08@gmail.com>
2022-01-24 12:34:30 -0500

    betty:__

--- functions_2.c
+++ functions_2.c
@@ -5,24 +5,24 @@
  * @head: Head of a double linked list
  * @line_number: Number of lines executed before an error is printed
  */
-// void _add(stack_t **head, unsigned int line_number)
-// {
-// 	int sum = 0;
-// 	stack_t *delete_node;
+void _add(stack_t **head, unsigned int line_number)
+{
+	int sum = 0;
+	stack_t *delete_node;
 
-// 	if ((*head) == NULL || (*head)->next == NULL)
-// 	{
-// 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-// 		exit(EXIT_FAILURE);
-// 	}
-// 	sum = (*head)->n + (*head)->next->n;
-// 	(*head)->next->n = sum;
+	if ((*head) == NULL || (*head)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	sum = (*head)->n + (*head)->next->n;
+	(*head)->next->n = sum;
 
-// 	delete_node = *head;
-// 	*head = delete_node->next;
-// 	if (*head != NULL)
-// 	{
-// 		(*head)->prev = NULL;
-// 	}
-// 	free(delete_node);
+	delete_node = *head;
+	*head = delete_node->next;
+	if (*head != NULL)
+	{
+		(*head)->prev = NULL;
+	}
+	free(delete_node);
 }
